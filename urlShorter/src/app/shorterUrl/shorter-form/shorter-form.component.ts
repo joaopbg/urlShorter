ccc8ca837bc89702c8c9d81ff4254906c4c306b8
Joao Pedro <joaopedrobarsaglini@gmail.com>
2022-03-26 22:59:24 -0300

    shorter-form constructor implemented

--- urlShorter/src/app/shorterUrl/shorter-form/shorter-form.component.ts
+++ urlShorter/src/app/shorterUrl/shorter-form/shorter-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BitlyService } from 'src/app/services/bitly.service';
 
 @Component({
   selector: 'app-shorter-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShorterFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bitlyService: BitlyService) { }
 
   ngOnInit(): void {
   }
