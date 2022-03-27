d05da1f6420770e212feea83849cbea54ee2b45a
Joao Pedro <joaopedrobarsaglini@gmail.com>
2022-03-26 22:58:17 -0300

    bitly service added

--- urlShorter/src/app/app.module.ts
+++ urlShorter/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShorterFormComponent } from './shorterUrl/shorter-form/shorter-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ urlShorter/src/app/services/bitly.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+  
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BitlyService {
+
+  private link : any
+
+  private token = '0077062cbffbc8362ee9ed3ba3f1cf0596493d7d'
+
+  constructor(private http : HttpClient) { }
+
+  public getLink(longLink : String):Observable<any>{
+    return this.http.get(`https://api-ssl.bitly.com/shorten?access_token=${this.token}&longUrl=${longLink}`)
+
+  }
+}
